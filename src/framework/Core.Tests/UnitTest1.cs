62c0317a5aa0534c58a4132964d963f5c8698ff3
Benjamin Gibbs <85989324+BenjaminLGibbs@users.noreply.github.com>
2025-01-29 22:11:57 +0000

    Add unit test project with initial test case

---
+++ src/framework/Core.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Mississippi.Core.Tests;
+
+/// <summary>
+/// A test class.
+/// </summary>
+public class UnitTest1
+{
+    /// <summary>
+    /// A test method.
+    /// </summary>
+    [Fact]
+    public void Test1()
+    {
+        Assert.Equal(1, 1);
+    }
+}
