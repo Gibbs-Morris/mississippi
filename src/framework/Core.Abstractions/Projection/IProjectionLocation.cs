43566a0eebea3aedc1ca7eb8c1275250c514a419
Test User <test@example.com>
2025-03-13 21:58:24 +0000

    Add IProjectionLocation and IReadonlyProjection interfaces for projection metadata

---
+++ src/framework/Core.Abstractions/Projection/IProjectionLocation.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Mississippi.Core.Abstractions.Projection;
+
+/// <summary>
+/// Defines the location metadata for a projection.
+/// </summary>
+public interface IProjectionLocation
+{
+    /// <summary>
+    /// Gets the path of the projection location.
+    /// </summary>
+    string Path { get; init; }
+
+    /// <summary>
+    /// Gets the identifier of the projection location.
+    /// </summary>
+    string Id { get; init; }
+}
