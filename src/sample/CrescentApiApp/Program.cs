af6275d48cbca2106d89b3d6fd63251e01aefbe2
Test User <test@example.com>
2025-06-24 00:54:11 +0100

    Add ConfigureAwait(false) to Main method for improved async performance

--- src/sample/CrescentApiApp/Program.cs
+++ src/sample/CrescentApiApp/Program.cs
@@ -20,6 +20,6 @@ internal static class Program
     {
         WebApplicationBuilder? builder = WebApplication.CreateBuilder(args);
         WebApplication? app = builder.Build();
-        await app.RunAsync();
+        await app.RunAsync().ConfigureAwait(false);
     }
 }
