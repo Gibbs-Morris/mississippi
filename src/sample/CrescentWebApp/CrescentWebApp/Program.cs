0e7ce7dc1970e5939557fef79bdf8f4dae385239
Test User <test@example.com>
2025-06-28 03:21:25 +0100

    Add initial Blazor WebAssembly app structure with error handling and routing.

---
+++ src/sample/CrescentWebApp/CrescentWebApp/Program.cs
@@ -0,0 +1,29 @@
+using Mississippi.CrescentWebApp.Components;
+
+using _Imports = Mississippi.CrescentWebApp.Client._Imports;
+
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents().AddInteractiveWebAssemblyComponents();
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", true);
+
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseAntiforgery();
+app.MapStaticAssets();
+app.MapRazorComponents<App>().AddInteractiveWebAssemblyRenderMode().AddAdditionalAssemblies(typeof(_Imports).Assembly);
+await app.RunAsync().ConfigureAwait(true);
