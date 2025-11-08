f37a7a83479af9ac34c91025e506e547865d80bb
Test User <test@example.com>
2025-11-08 22:46:33 +0000

    refactor: reorder blob lease interface members

--- src/EventSourcing.Cosmos/Locking/IBlobLeaseClient.cs
+++ src/EventSourcing.Cosmos/Locking/IBlobLeaseClient.cs
@@ -28,23 +28,23 @@ internal interface IBlobLeaseClient
     );
 
     /// <summary>
-    ///     Renews the existing lease associated with the blob.
-    /// </summary>
-    /// <param name="conditions">Optional request conditions.</param>
-    /// <param name="cancellationToken">A token to observe while waiting for the task to complete.</param>
-    /// <returns>A response containing the renewed <see cref="BlobLease" />.</returns>
-    Task<Response<BlobLease>> RenewAsync(
-        RequestConditions? conditions = null,
-        CancellationToken cancellationToken = default
-    );
-
-    /// <summary>
     ///     Releases the lease associated with the blob.
     /// </summary>
     /// <param name="conditions">Optional request conditions.</param>
     /// <param name="cancellationToken">A token to observe while waiting for the task to complete.</param>
     /// <returns>A response containing metadata about the release operation.</returns>
     Task<Response<ReleasedObjectInfo>> ReleaseAsync(
+        RequestConditions? conditions = null,
+        CancellationToken cancellationToken = default
+    );
+
+    /// <summary>
+    ///     Renews the existing lease associated with the blob.
+    /// </summary>
+    /// <param name="conditions">Optional request conditions.</param>
+    /// <param name="cancellationToken">A token to observe while waiting for the task to complete.</param>
+    /// <returns>A response containing the renewed <see cref="BlobLease" />.</returns>
+    Task<Response<BlobLease>> RenewAsync(
         RequestConditions? conditions = null,
         CancellationToken cancellationToken = default
     );
