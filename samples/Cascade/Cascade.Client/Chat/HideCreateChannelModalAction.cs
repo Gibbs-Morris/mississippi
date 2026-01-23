02ce1acfa0c97db5db14369285f6b0757c2c8a44
Test User <test@example.com>
2026-01-23 14:43:45 +0000

    Delete Cascade sample project files

--- samples/Cascade/Cascade.Client/Chat/HideCreateChannelModalAction.cs
+++
@@ -1,9 +0,0 @@
-using Mississippi.Reservoir.Abstractions.Actions;
-
-
-namespace Cascade.Client.Chat;
-
-/// <summary>
-///     Action dispatched to hide the create channel modal.
-/// </summary>
-internal sealed record HideCreateChannelModalAction : IAction;
