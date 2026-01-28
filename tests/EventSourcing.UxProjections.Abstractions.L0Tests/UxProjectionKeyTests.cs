aa3a79f1b7a73e154fb6a810091aebae9574f850
Test User <test@example.com>
2026-01-28 03:18:48 +0000

    chore: Remove Allure from UxProjectionKeyTests.cs

--- tests/EventSourcing.UxProjections.Abstractions.L0Tests/UxProjectionKeyTests.cs
+++ tests/EventSourcing.UxProjections.Abstractions.L0Tests/UxProjectionKeyTests.cs
@@ -1,6 +1,5 @@
 using System;
 
-using Allure.Xunit.Attributes;
 
 
 namespace Mississippi.EventSourcing.UxProjections.Abstractions.L0Tests;
@@ -8,9 +7,6 @@ namespace Mississippi.EventSourcing.UxProjections.Abstractions.L0Tests;
 /// <summary>
 ///     Tests for <see cref="UxProjectionKey" /> behavior.
 /// </summary>
-[AllureParentSuite("Event Sourcing")]
-[AllureSuite("UX Projections Abstractions")]
-[AllureSubSuite("UxProjectionKey")]
 public sealed class UxProjectionKeyTests
 {
     /// <summary>
