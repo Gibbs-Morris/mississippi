dce7d841a61fb4e9b2e6ab670b2c3d89c7d23cdb
Test User <test@example.com>
2026-01-28 03:18:18 +0000

    chore: Remove Allure from BrookAsyncReaderKeyTests.cs

--- tests/EventSourcing.Brooks.Abstractions.L0Tests/BrookAsyncReaderKeyTests.cs
+++ tests/EventSourcing.Brooks.Abstractions.L0Tests/BrookAsyncReaderKeyTests.cs
@@ -1,6 +1,5 @@
 using System;
 
-using Allure.Xunit.Attributes;
 
 
 namespace Mississippi.EventSourcing.Brooks.Abstractions.L0Tests;
@@ -8,9 +7,6 @@ namespace Mississippi.EventSourcing.Brooks.Abstractions.L0Tests;
 /// <summary>
 ///     Tests for <see cref="BrookAsyncReaderKey" /> behavior.
 /// </summary>
-[AllureParentSuite("Event Sourcing")]
-[AllureSuite("Brooks Abstractions")]
-[AllureSubSuite("Brook Async Reader Key")]
 public sealed class BrookAsyncReaderKeyTests
 {
     /// <summary>
