57ea14a8d2e6127abfece847fc865ecc96ba17c2
Test User <test@example.com>
2026-01-28 03:18:16 +0000

    chore: Remove Allure from SnapshotTypeRegistryTests.cs

--- tests/EventSourcing.Aggregates.L0Tests/SnapshotTypeRegistryTests.cs
+++ tests/EventSourcing.Aggregates.L0Tests/SnapshotTypeRegistryTests.cs
@@ -1,6 +1,5 @@
 using System;
 
-using Allure.Xunit.Attributes;
 
 
 namespace Mississippi.EventSourcing.Aggregates.L0Tests;
@@ -8,9 +7,6 @@ namespace Mississippi.EventSourcing.Aggregates.L0Tests;
 /// <summary>
 ///     Tests for <see cref="SnapshotTypeRegistry" />.
 /// </summary>
-[AllureParentSuite("Event Sourcing")]
-[AllureSuite("Aggregates")]
-[AllureSubSuite("Snapshot Type Registry")]
 public class SnapshotTypeRegistryTests
 {
     /// <summary>
