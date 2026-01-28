ce3926d4f4732a6e5f5629f3779d040ff2b56bba
Test User <test@example.com>
2026-01-28 03:18:12 +0000

    chore: Remove Allure from OperationResultTests.cs

--- tests/EventSourcing.Aggregates.Abstractions.L0Tests/OperationResultTests.cs
+++ tests/EventSourcing.Aggregates.Abstractions.L0Tests/OperationResultTests.cs
@@ -1,6 +1,5 @@
 using System;
 
-using Allure.Xunit.Attributes;
 
 
 namespace Mississippi.EventSourcing.Aggregates.Abstractions.L0Tests;
@@ -8,9 +7,6 @@ namespace Mississippi.EventSourcing.Aggregates.Abstractions.L0Tests;
 /// <summary>
 ///     Tests for <see cref="OperationResult" /> behavior.
 /// </summary>
-[AllureParentSuite("Event Sourcing")]
-[AllureSuite("Aggregates Abstractions")]
-[AllureSubSuite("Operation Result")]
 public class OperationResultTests
 {
     /// <summary>
