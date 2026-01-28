324f699decaea7782b8a5086812a676c2fb50a33
Test User <test@example.com>
2026-01-28 03:19:02 +0000

    chore: Remove Allure from AggregateModelTests.cs

--- tests/Inlet.Generators.Core.L0Tests/Analysis/AggregateModelTests.cs
+++ tests/Inlet.Generators.Core.L0Tests/Analysis/AggregateModelTests.cs
@@ -1,6 +1,5 @@
 using System;
 
-using Allure.Xunit.Attributes;
 
 using Microsoft.CodeAnalysis;
 
@@ -14,9 +13,6 @@ namespace Mississippi.Inlet.Generators.Core.L0Tests.Analysis;
 /// <summary>
 ///     Tests for <see cref="Core.Analysis.AggregateModel" />.
 /// </summary>
-[AllureParentSuite("SDK")]
-[AllureSuite("Generators Core")]
-[AllureSubSuite("Aggregate Model")]
 public class AggregateModelTests
 {
     /// <summary>
