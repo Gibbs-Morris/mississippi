a4c6b585a2918314dd8aaa14c0d672ec33cf7821
Test User <test@example.com>
2026-01-28 03:19:06 +0000

    chore: Remove Allure from AggregateSiloRegistrationGeneratorTests.cs

--- tests/Inlet.Silo.Generators.L0Tests/AggregateSiloRegistrationGeneratorTests.cs
+++ tests/Inlet.Silo.Generators.L0Tests/AggregateSiloRegistrationGeneratorTests.cs
@@ -4,7 +4,6 @@ using System.Collections.Immutable;
 using System.IO;
 using System.Linq;
 
-using Allure.Xunit.Attributes;
 
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
@@ -15,9 +14,6 @@ namespace Mississippi.Inlet.Silo.Generators.L0Tests;
 /// <summary>
 ///     Tests for <see cref="AggregateSiloRegistrationGenerator" />.
 /// </summary>
-[AllureParentSuite("SDK")]
-[AllureSuite("Silo Generators")]
-[AllureSubSuite("Aggregate Silo Registration Generator")]
 public class AggregateSiloRegistrationGeneratorTests
 {
     /// <summary>
