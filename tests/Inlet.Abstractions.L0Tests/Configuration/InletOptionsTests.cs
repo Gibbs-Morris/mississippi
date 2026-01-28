d8ac340bc39249618186872519c850c636821d2c
Test User <test@example.com>
2026-01-28 03:18:53 +0000

    chore: Remove Allure from InletOptionsTests.cs

--- tests/Inlet.Abstractions.L0Tests/Configuration/InletOptionsTests.cs
+++ tests/Inlet.Abstractions.L0Tests/Configuration/InletOptionsTests.cs
@@ -1,6 +1,5 @@
 using System;
 
-using Allure.Xunit.Attributes;
 
 using Mississippi.Inlet.Client.Abstractions.Configuration;
 
@@ -10,17 +9,13 @@ namespace Mississippi.Inlet.Abstractions.L0Tests.Configuration;
 /// <summary>
 ///     Tests for <see cref="InletOptions" />.
 /// </summary>
-[AllureParentSuite("Mississippi.Inlet.Abstractions")]
-[AllureSuite("Configuration")]
-[AllureSubSuite("InletOptions")]
 public sealed class InletOptionsTests
 {
     /// <summary>
     ///     Default AutoReconnect should be true.
     /// </summary>
     [Fact]
-    [AllureFeature("Defaults")]
-    public void AutoReconnectDefaultsToTrue()
+        public void AutoReconnectDefaultsToTrue()
     {
         // Act
         InletOptions sut = new();
@@ -33,8 +28,7 @@ public sealed class InletOptionsTests
     ///     AutoReconnect should be settable.
     /// </summary>
     [Fact]
-    [AllureFeature("Properties")]
-    public void AutoReconnectIsSettable()
+        public void AutoReconnectIsSettable()
     {
         // Arrange
         InletOptions sut = new();
@@ -50,8 +44,7 @@ public sealed class InletOptionsTests
     ///     Default DefaultTimeout should be 30 seconds.
     /// </summary>
     [Fact]
-    [AllureFeature("Defaults")]
-    public void DefaultTimeoutDefaultsTo30Seconds()
+        public void DefaultTimeoutDefaultsTo30Seconds()
     {
         // Act
         InletOptions sut = new();
@@ -64,8 +57,7 @@ public sealed class InletOptionsTests
     ///     DefaultTimeout should be settable.
     /// </summary>
     [Fact]
-    [AllureFeature("Properties")]
-    public void DefaultTimeoutIsSettable()
+        public void DefaultTimeoutIsSettable()
     {
         // Arrange
         InletOptions sut = new();
@@ -81,8 +73,7 @@ public sealed class InletOptionsTests
     ///     Default MaxReconnectAttempts should be 5.
     /// </summary>
     [Fact]
-    [AllureFeature("Defaults")]
-    public void MaxReconnectAttemptsDefaultsTo5()
+        public void MaxReconnectAttemptsDefaultsTo5()
     {
         // Act
         InletOptions sut = new();
@@ -95,8 +86,7 @@ public sealed class InletOptionsTests
     ///     MaxReconnectAttempts should be settable.
     /// </summary>
     [Fact]
-    [AllureFeature("Properties")]
-    public void MaxReconnectAttemptsIsSettable()
+        public void MaxReconnectAttemptsIsSettable()
     {
         // Arrange
         InletOptions sut = new();
@@ -112,8 +102,7 @@ public sealed class InletOptionsTests
     ///     Default ReconnectDelay should be 5 seconds.
     /// </summary>
     [Fact]
-    [AllureFeature("Defaults")]
-    public void ReconnectDelayDefaultsTo5Seconds()
+        public void ReconnectDelayDefaultsTo5Seconds()
     {
         // Act
         InletOptions sut = new();
@@ -126,8 +115,7 @@ public sealed class InletOptionsTests
     ///     ReconnectDelay should be settable.
     /// </summary>
     [Fact]
-    [AllureFeature("Properties")]
-    public void ReconnectDelayIsSettable()
+        public void ReconnectDelayIsSettable()
     {
         // Arrange
         InletOptions sut = new();
