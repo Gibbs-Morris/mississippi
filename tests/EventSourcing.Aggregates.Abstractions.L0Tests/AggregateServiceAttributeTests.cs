ba78f620c711a7e631d8082447b2ce867a71f29d
Test User <test@example.com>
2026-01-28 03:18:11 +0000

    chore: Remove Allure from AggregateServiceAttributeTests.cs

--- tests/EventSourcing.Aggregates.Abstractions.L0Tests/AggregateServiceAttributeTests.cs
+++ tests/EventSourcing.Aggregates.Abstractions.L0Tests/AggregateServiceAttributeTests.cs
@@ -1,6 +1,5 @@
 using System;
 
-using Allure.Xunit.Attributes;
 
 
 namespace Mississippi.EventSourcing.Aggregates.Abstractions.L0Tests;
@@ -8,17 +7,13 @@ namespace Mississippi.EventSourcing.Aggregates.Abstractions.L0Tests;
 /// <summary>
 ///     Tests for <see cref="AggregateServiceAttribute" /> behavior.
 /// </summary>
-[AllureParentSuite("Event Sourcing")]
-[AllureSuite("Aggregates Abstractions")]
-[AllureSubSuite("Aggregate Service Attribute")]
 public sealed class AggregateServiceAttributeTests
 {
     /// <summary>
     ///     Authorize property should default to null.
     /// </summary>
     [Fact]
-    [AllureFeature("Properties")]
-    public void AuthorizePropertyDefaultsToNull()
+        public void AuthorizePropertyDefaultsToNull()
     {
         // Arrange & Act
         AggregateServiceAttribute attribute = new("users");
@@ -31,8 +26,7 @@ public sealed class AggregateServiceAttributeTests
     ///     Authorize property should be settable.
     /// </summary>
     [Fact]
-    [AllureFeature("Properties")]
-    public void AuthorizePropertyIsSettable()
+        public void AuthorizePropertyIsSettable()
     {
         // Arrange
         AggregateServiceAttribute attribute = new("users");
@@ -48,8 +42,7 @@ public sealed class AggregateServiceAttributeTests
     ///     Constructor should succeed with valid route.
     /// </summary>
     [Fact]
-    [AllureFeature("Construction")]
-    public void ConstructorSucceedsWithValidRoute()
+        public void ConstructorSucceedsWithValidRoute()
     {
         // Arrange & Act
         AggregateServiceAttribute attribute = new("users");
@@ -62,8 +55,7 @@ public sealed class AggregateServiceAttributeTests
     ///     Constructor should throw ArgumentNullException when route is null.
     /// </summary>
     [Fact]
-    [AllureFeature("Argument Validation")]
-    public void ConstructorThrowsWhenRouteIsNull()
+        public void ConstructorThrowsWhenRouteIsNull()
     {
         // Arrange, Act & Assert
         Assert.Throws<ArgumentNullException>(() => new AggregateServiceAttribute(null!));
@@ -73,8 +65,7 @@ public sealed class AggregateServiceAttributeTests
     ///     GenerateApi property should default to true.
     /// </summary>
     [Fact]
-    [AllureFeature("Properties")]
-    public void GenerateApiPropertyDefaultsToTrue()
+        public void GenerateApiPropertyDefaultsToTrue()
     {
         // Arrange & Act
         AggregateServiceAttribute attribute = new("users");
@@ -87,8 +78,7 @@ public sealed class AggregateServiceAttributeTests
     ///     GenerateApi property should be settable.
     /// </summary>
     [Fact]
-    [AllureFeature("Properties")]
-    public void GenerateApiPropertyIsSettable()
+        public void GenerateApiPropertyIsSettable()
     {
         // Arrange
         AggregateServiceAttribute attribute = new("users");
@@ -104,8 +94,7 @@ public sealed class AggregateServiceAttributeTests
     ///     Route property should return the value passed to constructor.
     /// </summary>
     [Fact]
-    [AllureFeature("Properties")]
-    public void RoutePropertyReturnsConstructorValue()
+        public void RoutePropertyReturnsConstructorValue()
     {
         // Arrange & Act
         AggregateServiceAttribute attribute = new("orders");
