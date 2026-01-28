5dbe29996eef2a042cc73f824599e705ccba7f50
Test User <test@example.com>
2026-01-28 03:17:51 +0000

    chore: Remove Allure from Aqueduct.Abstractions.L0Keys/SignalRServerDirectoryKeyTests.cs

--- tests/Aqueduct.Abstractions.L0Tests/Keys/SignalRServerDirectoryKeyTests.cs
+++ tests/Aqueduct.Abstractions.L0Tests/Keys/SignalRServerDirectoryKeyTests.cs
@@ -1,6 +1,5 @@
 using System;
 
-using Allure.Xunit.Attributes;
 
 using Mississippi.Aqueduct.Abstractions.Keys;
 
@@ -10,9 +9,6 @@ namespace Mississippi.Aqueduct.Abstractions.L0Tests.Keys;
 /// <summary>
 ///     Tests for <see cref="SignalRServerDirectoryKey" />.
 /// </summary>
-[AllureParentSuite("ASP.NET Core")]
-[AllureSuite("SignalR Orleans")]
-[AllureSubSuite("Server Directory Key")]
 public sealed class SignalRServerDirectoryKeyTests
 {
     /// <summary>
