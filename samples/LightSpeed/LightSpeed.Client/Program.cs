a4f8a17e2dcf6bae8c46c820d785c8ee61e17a96
Test User <test@example.com>
2026-02-01 21:31:58 +0000

    Apply code cleanup formatting

--- samples/LightSpeed/LightSpeed.Client/Program.cs
+++ samples/LightSpeed/LightSpeed.Client/Program.cs
@@ -2,6 +2,7 @@ using LightSpeed.Client;
 
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
 using Mississippi.Reservoir;
 using Mississippi.Reservoir.Blazor;
 
@@ -9,8 +10,6 @@ using Mississippi.Reservoir.Blazor;
 WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
 builder.Services.AddReservoir();
 builder.Services.AddReservoirDevTools();
-
 await builder.Build().RunAsync();
