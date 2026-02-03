cff62b5e7f34d26d6468da2c022810d620de9983
Test User <test@example.com>
2026-02-03 05:38:00 +0000

    Wire Reservoir built-in navigation for LightSpeed +semver: skip

--- samples/LightSpeed/LightSpeed.Client/Program.cs
+++ samples/LightSpeed/LightSpeed.Client/Program.cs
@@ -5,11 +5,18 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 using Mississippi.Reservoir;
 using Mississippi.Reservoir.Blazor;
+using Mississippi.Reservoir.Blazor.BuiltIn;
 
 
 WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddReservoir();
-builder.Services.AddReservoirDevTools();
+builder.Services.AddReservoirBlazorBuiltIns();
+builder.Services.AddReservoirDevTools(options =>
+{
+    options.Enablement = ReservoirDevToolsEnablement.Always;
+    options.Name = "Lightspeed Sample";
+    options.IsStrictStateRehydrationEnabled = true;
+});
 await builder.Build().RunAsync();
