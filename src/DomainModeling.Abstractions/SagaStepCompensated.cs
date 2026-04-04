8e4d5427d7ee6d80c2b90efc6e11b5d720a8ec63
Test User <test@example.com>
2026-04-04 04:35:22 +0100

    Version saga step compensated storage name

--- src/DomainModeling.Abstractions/SagaStepCompensated.cs
+++ src/DomainModeling.Abstractions/SagaStepCompensated.cs
@@ -12,7 +12,7 @@ namespace Mississippi.DomainModeling.Abstractions;
 /// </summary>
 [GenerateSerializer]
 [Alias("Mississippi.DomainModeling.Abstractions.SagaStepCompensated")]
-[EventStorageName("MISSISSIPPI", "SAGAS", "SAGASTEPCOMPENSATED")]
+[EventStorageName("MISSISSIPPI", "SAGAS", "SAGASTEPCOMPENSATED", version: 1)]
 public sealed record SagaStepCompensated
 {
     /// <summary>
