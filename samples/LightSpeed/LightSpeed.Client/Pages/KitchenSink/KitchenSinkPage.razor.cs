45a7f1c4ab7c5050d26f1e2981723b2a902dffc0
Benjamin Gibbs <85989324+BenjaminLGibbs@users.noreply.github.com>
2026-09-10 06:29:17 +0100

    Declare kitchen inheritance once for review #3975685466

--- samples/LightSpeed/LightSpeed.Client/Pages/KitchenSink/KitchenSinkPage.razor.cs
+++ samples/LightSpeed/LightSpeed.Client/Pages/KitchenSink/KitchenSinkPage.razor.cs
@@ -1,5 +1,4 @@
 using Mississippi.Refraction.Client.Infrastructure.Theming;
-using Mississippi.Reservoir.Client;
 
 using MississippiSamples.LightSpeed.Client.Features.Showcase;
 
@@ -8,7 +7,7 @@ namespace MississippiSamples.LightSpeed.Client.Pages.KitchenSink;
 
 /// <summary>Connects the interactive gallery to Reservoir actions and selectors.</summary>
 /// <remarks>Public so the Blazor router can instantiate the page.</remarks>
-public sealed partial class KitchenSinkPage : StoreComponent
+public sealed partial class KitchenSinkPage
 {
     private ShowcaseView View => Select<ShowcaseState, ShowcaseView>(ShowcaseSelectors.GetView);
 
